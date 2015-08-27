f3356b7ef4f5e65ecdba321c4305f4dd8f3f2bdf
Mark Baldwin Martinez <mbmartinez@exist.com>
2015-08-27 10:29:09 +0800

    fix email

--- pabili-core/pabili-core-service/src/main/java/com/pabili/core/config/MailConfig.java
+++ pabili-core/pabili-core-service/src/main/java/com/pabili/core/config/MailConfig.java
@@ -2,46 +2,44 @@ package com.pabili.core.config;
 
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @PropertySource("classpath:mail.properties")
 public class MailConfig {
 
-    @Value("${email.host}")
-    private String host;
-
-    @Value("${email.port}")
-    private Integer port;
-
-    @Value("${email.username}")
-    private String username;
-
-    @Value("${email.password}")
-    private String password;
+    @Autowired
+    private Environment env;
 
     @Bean
     public JavaMailSender javaMailService() {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost(host);
-        javaMailSender.setPort(port);
-        javaMailSender.setUsername(username);
-        javaMailSender.setPassword(password);
-        javaMailSender.setJavaMailProperties(getMailProperties());
-        return javaMailSender;
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", env.getProperty("mail.smtp.auth"));
+        mailProperties.put("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable"));
+        mailSender.setJavaMailProperties(mailProperties);
+        mailSender.setHost(env.getProperty("mail.host"));
+        mailSender.setPort(env.getProperty("mail.port", Integer.class));
+        mailSender.setProtocol(env.getProperty("mail.protocol"));
+        mailSender.setUsername(env.getProperty("mail.username"));
+        mailSender.setPassword(env.getProperty("mail.password"));
+        return mailSender;
     }
 
-    private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("mail.transport.protocol", "smtp");
-        properties.setProperty("mail.smtp.auth", "true");
-        properties.setProperty("mail.smtp.starttls.enable", "true");
-        properties.setProperty("mail.debug", "false");
-        return properties;
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setQueueCapacity(100);
+        return taskExecutor;
     }
 }
--- pabili-core/pabili-core-service/src/main/java/com/pabili/core/service/custom/impl/PabiliMailSenderCustomImpl.java
+++ pabili-core/pabili-core-service/src/main/java/com/pabili/core/service/custom/impl/PabiliMailSenderCustomImpl.java
@@ -1,15 +1,15 @@
 package com.pabili.core.service.custom.impl;
 
-import java.util.Properties;
-
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.env.Environment;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import com.pabili.core.service.custom.PabiliMailSender;
@@ -20,29 +20,52 @@ public class PabiliMailSenderCustomImpl implements PabiliMailSender {
     private static Logger LOG = LoggerFactory.getLogger(PabiliMailSenderCustomImpl.class);
 
     @Autowired
-    private JavaMailSender mailSender;
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private TaskExecutor taskExecutor;
+
+    private class EmailNotificationTask implements Runnable {
+
+        private String emailTo;
+        private String subject;
+        private String text;
+
+        public EmailNotificationTask(String emailTo, String subject, String text) {
+            this.emailTo = emailTo;
+            this.subject = subject;
+            this.text = text;
+        }
+
+        @Override
+        public void run() {
+            try {
+                //Prepare email details
+                MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+                MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                message.setSubject(subject);
+                message.setFrom(env.getProperty("pabili.com.ph@gmail.com"));
+                message.setTo(emailTo);
+
+                message.setText(text, true);
+                javaMailSender.send(mimeMessage);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void sendRegistrationToken(String email, String registrationToken) {
         LOG.debug("Sending registration token. email={}", email);
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("pabili.com.ph");
-        message.setTo(email);
-        message.setSubject("Welcome to pabili.com.ph! Please complete your registration.");
-        message.setText("Dude. Testerino. token=" + registrationToken);
-        mailSender.send(message);
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
+        EmailNotificationTask task = new EmailNotificationTask(
+                email,
+                "Welcome to pabili.com.ph! Please complete your registration.",
+                "Hello world");
+        taskExecutor.execute(task);
     }
 
 }
