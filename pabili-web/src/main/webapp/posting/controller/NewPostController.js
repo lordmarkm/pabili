76f8bd741e0b2a3d67cf428f6a9fc9999ed12b07
Mark Baldwin Martinez <mbmartinez@exist.com>
2015-09-03 17:48:10 +0800

    good stuff

--- pabili-web/src/main/webapp/posting/controller/NewPostController.js
+++ pabili-web/src/main/webapp/posting/controller/NewPostController.js
@@ -1,8 +1,17 @@
 define(function () {
-  return ['$scope', 'PostingService',
-    function ($scope, PostingService) {
+  return ['$scope', '$state', 'PostingService',
+    function ($scope, $state, PostingService) {
 
     $scope.post = {};
+    $scope.invalidDetails = function () {
+      if (!$scope.post.title || $scope.post.title.length < 10) return true;
+      if (!$scope.post.description || $scope.post.description.length < 40) return true;
+      if (!$scope.post.price || $scope.post.price < 1) return true;
+      return false;
+    };
+    $scope.invalidLocation = function () {
+      if (!$scope.post.location) return true;
+    };
 
   }];
 });
