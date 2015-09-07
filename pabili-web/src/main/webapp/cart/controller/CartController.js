5d05a63daed3e38753758e34e0ed1f962d9708f9
Mark Baldwin Martinez <mbmartinez@exist.com>
2015-09-07 18:19:28 +0800

    More work on shopping cart

--- pabili-web/src/main/webapp/cart/controller/CartController.js
+++ pabili-web/src/main/webapp/cart/controller/CartController.js
@@ -2,5 +2,12 @@ define(function () {
   return ['$scope',
     function ($scope) {
 
+    $scope.decrement = function (buyRequest, index) {
+      buyRequest.quantity = buyRequest.quantity - 1;
+      if (buyRequest.quantity <= 0) {
+        $scope.cart.buyRequests.splice(index, 1);
+      }
+    };
+
   }];
 });
